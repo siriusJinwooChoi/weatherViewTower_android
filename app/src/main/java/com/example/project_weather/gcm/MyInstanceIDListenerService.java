ea94f9c8df088824d57ce860875d49bcfdfeec1f
JinWoo Choi <indra4812@gmail.com>
2016-07-08 20:25:48 +0900

    Update MyInstanceIDListenerService.java

--- app/src/main/java/com/example/project_weather/gcm/MyInstanceIDListenerService.java
+++ app/src/main/java/com/example/project_weather/gcm/MyInstanceIDListenerService.java
@@ -3,7 +3,7 @@ package com.example.project_weather.gcm;
 Made by : Jinwoo Choi,
 Location : Suwon, Korea.
 Date    : 2016.04.12
-Major   : Hankuk University of Foreign Studies, ICE(Information Communication Engineering,  201003629
+Major   : Hankuk University of Foreign Studies, ICE(Information Communication Engineering)
  */
 import android.content.Intent;
 
