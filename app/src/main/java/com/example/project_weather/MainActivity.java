92b23440353dbb4b9747ed735d66f82f09a40d43
JinWoo Choi <indra4812@gmail.com>
2016-07-08 20:16:19 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/project_weather/MainActivity.java
+++ app/src/main/java/com/example/project_weather/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.project_weather;
 Made by : Jinwoo Choi,
 Location : Suwon, Korea.
 Date    : 2016.04.12
-Major   : Hankuk University of Foreign Studies, ICE(Information Communication Engineering
+Major   : Hankuk University of Foreign Studies, ICE(Information Communication Engineering)
  */
 
 import android.app.Activity;
